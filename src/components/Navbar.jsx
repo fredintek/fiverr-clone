d09c70e413f6662ee436862785391e357cb15a9f
Alfred Arinze <fredintek@gmail.com>
2023-05-03 17:53:30 +0300

    navbar in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <span>
+            <span className="text">Fiverr</span>
+            <span className="dots">.</span>
+          </span>
+        </div>
+        <div className="links"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
